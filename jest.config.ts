aceed5f4e67ecb8c8f2eb8d694c26189c8d540fc
gzeal <unovan@protonmail.com>
2024-06-01 14:33:51 -0400

    Working on Jest implimentation

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+// Export a Jest configuration object
+export default {
+  // Set the preset to 'ts-jest' for TypeScript support
+  preset: 'ts-jest',
+  // Specify the test environment as 'jsdom' to simulate a browser-like environment
+  testEnvironment: 'jsdom',
+  // Define module name mapping for CSS, LESS, SCSS, and SASS files to 'identity-obj-proxy'
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  // Specify setup files to be run after Jest is initialized
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+};
+
+// may need to look into "jest.pathToJest": "npm.cmd test --"
+// run "npx jest" to run the tests or npx jest __tests__/name.test.ts etc
+// ?may also Ctrl+Shift+P if installed on VScode and search Run Test?
