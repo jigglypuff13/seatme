ef75d710fcb3aeb5a263f423de76c332c6d4d5e5
heymistersin <99973059+heymistersin@users.noreply.github.com>
2024-06-05 19:55:25 -0400

    attempting to export ts ext to other files

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,25 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 // const path = require('path');
 import path from 'path';
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config(); // crucial
-
-const connectionString: string = process.env.CONNECTION_STRING
-const client = new Pool({
-  connectionString,
-});
-
-const connectDB = async () => {
-  try {
-    await client.connect();
-    console.log('Connected to ElephantSQL database');
-  } catch (error) {
-    console.error('Failed to connect to the database', error);
-  }
-};
-
+import connectDB from './database.ts';
 
 const app = express();
 
