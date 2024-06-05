7a4d686ac94ebcc8a49f29447500fd0ee5d718ed
heymistersin <99973059+heymistersin@users.noreply.github.com>
2024-06-05 17:18:32 -0400

    added global handler. bug: catch all 404 page not rendering on browser

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 // const path = require('path');
 import path from 'path';
-import { Client } from 'pg';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config(); // crucial
 
 const connectionString: string = process.env.CONNECTION_STRING
-const client = new Client({
+const client = new Pool({
   connectionString,
 });
 
@@ -25,6 +25,27 @@ const app = express();
 
 connectDB();
 
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.use('/', (req: Request, res: Response) => res.sendFile(path.join(__dirname, '../client/index.html')))
+
+
+// CURRENTLY NOT WORKING
+// /* 404 Catch All */
+// app.all('*', (req: Request, res: Response) => {
+//   res.status(404).sendFile(path.join(__dirname, '../client/404.html'));
+//   // res.status(404).send('404: Page Not Found');
+// });
+//
+// // app.use((req: Request, res: Response, next: NextFunction) => {
+// //   res.status(404).send('404: Page Not Found');
+// // });
+// // CURRENTLY NOT WORKING
+
+
+/* Global Middlware Error Handling*/
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const defaultErr = {
+    log: 'Unknown middlware error'
+  }
+})
 
 app.listen(3000, () => console.log('server is listening on port 3000'));
