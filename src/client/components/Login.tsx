6afbd7ba79945483d428a5dabd5724ac7af8c747
Myles Austin <mtv1243@gmail.com>
2024-06-06 22:16:57 -0400

    add buttons to post data to either login or signup. The actual fetch is commented out until we have the routes set up in the server.

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,23 +1,55 @@
 import React, { useState } from 'react';
+import { LoginOrSignupPath } from '../../types';
 
-interface LoginProps extends React.HTMLAttributes<HTMLElement> {
-    email: string,
-    setEmail: React.Dispatch<React.SetStateAction<String>>,
-    password: string 
-    setPassword: React.Dispatch<React.SetStateAction<String>>,
+// interface LoginProps extends React.HTMLAttributes<HTMLElement> {
+//     email: string,
+//     setEmail: React.Dispatch<React.SetStateAction<String>>,
+//     password: string 
+//     setPassword: React.Dispatch<React.SetStateAction<String>>,
+// }
+
+
+
+
+const handleLoginOrSignup = (
+    path:LoginOrSignupPath, 
+    email:string,
+    setEmail:React.Dispatch<React.SetStateAction<String>>,
+    password:string,
+    setPassword:React.Dispatch<React.SetStateAction<String>>
+) => {
+    
+    console.log('post to ', path);
+    setEmail('');
+    setPassword('');
+
+    /*
+    fetch(path, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    })
+    // .then(res => res.json)
+    .then(res => {
+
+        const json = res.json();
+        console.log(json);
+        setEmail('');
+        setPassword('');
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    */
 }
 
 
-
-
-const Login: React.FC = (
-//     {
-//     email,
-//     setEmail,
-//     password,
-//     setPassword
-// }
-) => {
+const Login: React.FC = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -44,6 +76,28 @@ const Login: React.FC = (
                     }}
                     value={password}
                 />
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleLoginOrSignup(
+                        '/login', 
+                        email,
+                        setEmail,
+                        password,
+                        setPassword
+                        )}}
+                >Log in</button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleLoginOrSignup(
+                        '/signup', 
+                        email,
+                        setEmail,
+                        password,
+                        setPassword
+                        )}}
+                >Sign up</button>
             </form>
         </div>
     )
